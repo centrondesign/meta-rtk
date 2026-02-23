db7a178047889ee7c36096d6f6cca64abd532a1c
Jonathan Chang <jonathan.chang@centron.tw>
2026-02-23 18:53:52 +0800

    update trixie rootfs

--- recipes-avengers/prebuilt-rootfs/files/trixie-rootfs.sh
+++ recipes-avengers/prebuilt-rootfs/files/trixie-rootfs.sh
@@ -3,25 +3,47 @@
 debootstrap_rootfs() {
     sudo debootstrap \
         --arch=arm64 \
-        --include=ca-certificates,tzdata,locales,systemd-sysv,net-tools,command-not-found,sudo,nano,dbus,wpasupplicant,network-manager \
         trixie \
         arm64-rootfs \
         http://deb.debian.org/debian
 }
 
 chroot_fs() {
-    sudo chroot arm64-rootfs /bin/sh<<EOF
+    sudo mount --bind /dev arm64-rootfs/dev
+    sudo mount --bind /dev/pts arm64-rootfs/dev/pts
+    sudo mount --bind /proc arm64-rootfs/proc
+    sudo mount --bind /sys arm64-rootfs/sys
+
+    cat > arm64-rootfs/tmp/post_debootstrap <<EOF
 export LANG=C
+export LC_ALL=C
+
+dpkg --configure -a
+
+apt update
+
+apt install -y sudo nano net-tools locales tzdata ca-certificates systemd-sysv
+dpkg-reconfigure locales
+dpkg-reconfigure tzdata
+
+apt install -y dbus wpasupplicant network-manager
+sed -i -e '/managed=false/s/managed=false/managed=true/' /etc/NetworkManager/NetworkManager.conf
 
 echo "localhost" > /etc/hostname
 
-sed '/root/s/:[^:]*:/::/' /etc/shadow > /etc/shadow-
-cp /etc/shadow- /etc/shadow
+sed -i -e '/root/s/:[^:]*:/::/' /etc/shadow
 
 useradd -d /home/debian -m -g users -s /bin/bash -G sudo,audio,video,render debian
-sed '/debian/s/:[^:]*:/::/' /etc/shadow > /etc/shadow-
-cp /etc/shadow- /etc/shadow
+sed -i -e '/debian/s/:[^:]*:/::/' /etc/shadow
 EOF
+    chmod +x arm64-rootfs/tmp/post_debootstrap
+    sudo chroot arm64-rootfs /bin/bash -c /tmp/post_debootstrap
+    rm -f arm64-rootfs/tmp/post_debootstrap
+
+    sudo umount arm64-rootfs/sys
+    sudo umount arm64-rootfs/proc
+    sudo umount arm64-rootfs/dev/pts
+    sudo umount arm64-rootfs/dev
 }
 
 generate_tarball() {
